386d260ad6a63c88845d93fa3bcee2ec1a332d72
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-07 00:53:13 -0700

    npm i axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import{ BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
 import './App.css';
+
 
 class App extends Component {
   state = {
     todos: [
       {
-        id:1,
+        id: 1,
         title: 'Take out the trash',
         completed:false
       },
@@ -53,14 +56,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={ this.markComplete} delTodo={ this.delTodo}/>
-
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={ this.markComplete} delTodo={ this.delTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
